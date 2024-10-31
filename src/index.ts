ac27e460de08338ab974df5a6a06b18a97ea25f7
Maelys-e <135099544+Maelys-e@users.noreply.github.com>
2024-10-31 10:57:42 +0100

    Update index.ts
    Modification de index.js pour correspondre à ce qu'il y a sur mon fichier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
+import http from 'http';
 import { helloWorld } from './hello-world';
+import { helloDude } from './application';
+import { getSystemInformation } from './application';
 
-const greet = helloWorld();
-console.log(greet);
+//const greet = helloWorld();
+//console.log(greet);
+
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/api/v1/sysinfo') {
+        const sysinfo = getSystemInformation();
+        // on récupère la variable depuis application.ts
+        console.log('Server créé');
+        // on affiche dans la console pour contrôler
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        // on affiche sur la page web
+        res.end(JSON.stringify(sysinfo));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('Erreur 404');
+    }
+})
+
+
+server.listen(PORT, () => {
+    console.log('Serveur en écoute sur http://localhost/api/v1/sysinfo');
+})
